122c7cbbd9ebed89939dbc18c5d3ee51375519f0
Arvitheslayer <89351600+Arvitheslayer@users.noreply.github.com>
2022-05-05 22:25:40 +0200

    added some films,actors by admin panel.Created first html page and urls.

--- cinemaproject/urls.py
+++ cinemaproject/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls import static
 from django.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('movies/urls')),
+    path('', include('movies.urls')),
 ]
 
 
